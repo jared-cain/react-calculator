a8d6d400f19b8c91e725b783b26602302d91a04c
Jared Cain <jared.cain77@gmail.com>
2016-11-02 16:52:00 -0700

    add root route

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 4321;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function (req, res) {
+    res.status('200').send('Service is up');
+});
 
 app.listen(server_port, server_ip_address);
 

2083d90e39f2c7f1246d3f38179ba6a2f81e60dd
Jared Cain <jared.cain77@gmail.com>
2016-11-02 15:28:48 -0700

    update start script and server.js for openshift

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
-var port = 4321;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 4321;
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(port);
+app.listen(server_port, server_ip_address);
 
-console.log(`Running on port ${port}`);
+console.log(`Running on port ${server_port}`);
